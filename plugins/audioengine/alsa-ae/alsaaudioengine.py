119cd49c8cfd3284ed3879caf0eda2cbbd2fa0a4
Thomas Delteil <thomas.delteil1@gmail.com>
2016-03-26 16:12:14 +0000

    update

--- plugins/audioengine/alsa-ae/alsaaudioengine.py
+++ plugins/audioengine/alsa-ae/alsaaudioengine.py
@@ -114,7 +114,7 @@ class AlsaAudioDevice(plugin.audioengine.AudioDevice):
         pcm_type = alsaaudio.PCM_PLAYBACK if output else alsaaudio.PCM_CAPTURE
         stream = alsaaudio.PCM(type=pcm_type,
                                mode=alsaaudio.PCM_NORMAL,
-                               device=self.name)
+                               device='default' if output else self.name)
         stream.setchannels(channels)
         stream.setrate(rate)
         stream.setformat(bits_to_samplefmt(bits))
